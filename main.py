1a1d365a6a8a930f377000e9013948675286d1d6
Георгий Егоров <georgmix300@mail.ru>
2022-03-19 20:10:42 +0400

    App fix

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ import sys
 
 from PyQt5 import QtWidgets
 
-# from AdminWindow import AdminWindow
+from AdminWindow import AdminWindow
 from UserWindow import UserWindow
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui_1 = UserWindow()
-    ui_1.setupUi(MainWindow)
-    MainWindow.show()
+    MainWindow_1 = QtWidgets.QMainWindow()
+    MainWindow_2 = QtWidgets.QMainWindow()
+    ui_1 = AdminWindow()
+    ui_2 = UserWindow()
+    ui_1.setupUi(MainWindow_1)
+    ui_2.setupUi(MainWindow_2)
+    MainWindow_1.show()
+    MainWindow_2.show()
     sys.exit(app.exec_())
