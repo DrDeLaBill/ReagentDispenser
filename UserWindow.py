dfc1fd7d53ef37d094ed3d1d9409ae1b9662477e
Георгий Егоров <georgmix300@mail.ru>
2022-03-20 11:07:05 +0400

    user windows view logic change

--- UserWindow.py
+++ UserWindow.py
@@ -10,7 +10,7 @@ class UserWindow(Ui_MainWindow):
     def setupUi(self, MainWindow):
         # Main
         self.isGPIO = False
-        self.firstHand = False
+        self.isWarningTemp = False
         self.waitForSecondHand = False
         self.delayTime = time.time()
 
@@ -31,30 +31,48 @@ class UserWindow(Ui_MainWindow):
         self.isGPIO = AdminWindow.getWorkStatus()
 
     def checkVisitor(self):
-        distance = AdminWindow.getDistance()
-        if distance > constants.DISTANCE_MAX_VALUE and self.firstHand:
-            self.waitForSecondHand = True
-        if distance < constants.DISTANCE_MAX_VALUE:
+        if AdminWindow.getDistance() < constants.DISTANCE_MAX_VALUE:
             self.checkUserTemperature()
         elif not self.isDelayTime():
             self.showMainScreen()
 
     def checkUserTemperature(self):
-        if self.isAlertTemperature() and not self.isDelayTime():
-            self.showWarningScreen()
-            self.firstHand = True
-        elif self.isAlertTemperature() and self.isWaitForSecondHand():
-            self.showAlertScreen()
-            self.firstHand = False
+        if self.isWaitForSecondHand():
+            self.waitForSecondHand = True
+        elif self.isAlertTemperature():
+            self.delayTime = time.time()
             self.waitForSecondHand = False
-        else:
+            self.isWarningTemp = False
+            self.showAlertScreen()
+        elif self.isWarningTemperature():
+            self.delayTime = time.time()
+            self.isWarningTemp = True
+            self.waitForSecondHand = False
+            self.showWarningScreen()
+        elif self.isNormalTemperature():
+            self.delayTime = time.time()
+            self.isWarningTemp = False
+            self.waitForSecondHand = False
             self.showSuccesScreen()
+        elif not self.isDelayTime():
+            self.waitForSecondHand = False
+            self.isWarningTemp = False
+            self.showMainScreen()
+
+    def isWaitForSecondHand(self):
+        return self.isWarningTemp and AdminWindow.distance > constants.DISTANCE_MAX_VALUE and self.isDelayTime()
+
+    def isAlertTemperature(self):
+        return self.isWarningTemp and self.waitForSecondHand and AdminWindow.distance < constants.DISTANCE_MAX_VALUE and AdminWindow.temperature > constants.TEMPERATURE_MAX_VALUE
+
+    def isWarningTemperature(self):
+        return AdminWindow.distance < constants.DISTANCE_MAX_VALUE and AdminWindow.temperature > constants.TEMPERATURE_MAX_VALUE
+
+    def isNormalTemperature(self):
+        return AdminWindow.distance < constants.DISTANCE_MAX_VALUE and AdminWindow.temperature < constants.TEMPERATURE_MAX_VALUE
 
     def isDelayTime(self):
         return time.time() - self.delayTime < constants.TEMPERATURE_DELAY
-
-    def isWaitForSecondHand(self):
-        return self.firstHand and self.waitForSecondHand
 
     def isAlertTemperature(self):
         temperature = AdminWindow.getTemperature()
@@ -72,7 +90,6 @@ class UserWindow(Ui_MainWindow):
         self.mainBox.hide()
         self.alertBox.hide()
         self.successBox.hide()
-        self.delayTime = time.time()
         self.warningBox.show()
 
     def showAlertScreen(self):
@@ -85,7 +102,6 @@ class UserWindow(Ui_MainWindow):
         self.mainBox.hide()
         self.warningBox.hide()
         self.alertBox.hide()
-        self.delayTime = time.time()
         self.successBox.show()
 
     def hideAll(self):
